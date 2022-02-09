d986b9f46c606424ca3708911aa1c246a68607e9
Pedro Zago <pedro.camargozago@hotmail.com.br>
2022-02-09 09:38:23 -0300

    Assets

--- src/index.tsx
+++ src/index.tsx
@@ -12,19 +12,19 @@ createServer({
       transactions: [
         {
           id: 1,
-          title: 'Freelance de website',
+          title: 'Vendas online',
           type: 'deposit',
-          category: 'Dev',
-          amount: 6000,
-          createdAt: new Date('2021-02-12 09:00:00'),
+          category: 'Ecommerce',
+          amount: 8530,
+          createdAt: new Date('2022-02-07 09:00:00'),
         },
         {
           id: 2,
-          title: 'Aluguel',
+          title: 'Compra de material',
           type: 'withdraw',
-          category: 'Casa',
-          amount: 1100,
-          createdAt: new Date('2021-02-14 09:00:00'),
+          category: 'Materiais',
+          amount: 2380,
+          createdAt: new Date('2022-02-08 09:00:00'),
         }
       ]
     })
